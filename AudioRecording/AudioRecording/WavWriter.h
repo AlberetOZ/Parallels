f0da3153ec02ccecd98d15eee5affba62fb0afea
AlberetOZ <tver14judo@gmail.com>
2018-10-12 13:20:54 +0300

    english comments

--- AudioRecording/AudioRecording/WavWriter.h
+++ AudioRecording/AudioRecording/WavWriter.h
@@ -1,78 +1,60 @@
 
-// Класс, описывающий заголовок WAV файла.
 class WAV
 {
 public:
-	// WAV-формат начинается с RIFF-заголовка:
-
-	// Содержит символы "RIFF" в ASCII кодировке
-	// (0x52494646 в big-endian представлении)
+	// "RIFF" ASCII
+	// (0x52494646 in big-endian)
 	char chunkId[STRING_NUMBER_SYMBOLS_WAV];
 
-	// 36 + subchunk2Size, или более точно:
+	// 36 + subchunk2Size:
 	// 4 + (8 + subchunk1Size) + (8 + subchunk2Size)
-	// Это оставшийся размер цепочки, начиная с этой позиции.
-	// Иначе говоря, это размер файла - 8, то есть,
-	// исключены поля chunkId и chunkSize.
+	// size of file - 8
+	// without chunkId and chunkSize.
 	unsigned long chunkSize;
 
-	// Содержит символы "WAVE"
-	// (0x57415645 в big-endian представлении)
+	// "WAVE"
+	// (0x57415645 in big-endian)
 	char format[STRING_NUMBER_SYMBOLS_WAV];
 
-	// Формат "WAVE" состоит из двух подцепочек: "fmt " и "data":
-	// Подцепочка "fmt " описывает формат звуковых данных:
-
 	// Содержит символы "fmt "
-	// (0x666d7420 в big-endian представлении)
+	// (0x666d7420 in big-endian)
 	char subchunk1Id[STRING_NUMBER_SYMBOLS_WAV];
 
-	// 16 для формата PCM.
-	// Это оставшийся размер подцепочки, начиная с этой позиции.
+	// 16 for format PCM.
 	unsigned long subchunk1Size;
 
-	// Аудио формат
-	// Для PCM = 1 (то есть, Линейное квантование).
-	// Значения, отличающиеся от 1, обозначают некоторый формат сжатия.
+	// for PCM = 1.
 	unsigned short audioFormat;
 
-	// Количество каналов. Моно = 1, Стерео = 2 и т.д.
 	unsigned short numChannels;
 
-	// Частота дискретизации. 8000 Гц, 44100 Гц и т.д.
 	unsigned long sampleRate;
 
 	// sampleRate * numChannels * bitsPerSample/8
 	unsigned long byteRate;
 
 	// numChannels * bitsPerSample/8
-	// Количество байт для одного сэмпла, включая все каналы.
 	unsigned short blockAlign;
 
-	// Так называемая "глубиная" или точность звучания. 8 бит, 16 бит и т.д.
 	unsigned short bitsPerSample;
 
-	// Подцепочка "data" содержит аудио-данные и их размер.
-
 	// Содержит символы "data"
-	// (0x64617461 в big-endian представлении)
+	// (0x64617461 in big-endian)
 	char subchunk2Id[STRING_NUMBER_SYMBOLS_WAV];
 
 	// numSamples * numChannels * bitsPerSample/8
-	// Количество байт в области данных.
 	unsigned long subchunk2Size;
 	
-	// Далее следуют непосредственно Wav данные.
 	const short int* waveData;
 
-	WAV();													//	конструктор
-	WAV(const short int*, unsigned long);					// конструктор с данными
-	~WAV();													//	деструктор
+	WAV();													// constructor
+	WAV(const short int*, unsigned long);					// constructor with data
+	~WAV();													// destructor
 
-	int read(const short int*, unsigned long);			// данные в класс
-	int write(FILE*);						// данные в файл
-	int count();										// считает byteRate, blockAlign, chunkSize
-	//int strncpy(char*, const size_t, const char*);		// собственная strncpy, так как требуется работа без \0
+	int read(const short int*, unsigned long);				// data in class
+	int write(FILE*);										// data in file 
+	int count();											// count byteRate, blockAlign, chunkSize
+	//int strncpy(char*, const size_t, const char*);		// my strncpy, because need work without \0
 };
 
 int WAV::count() {
@@ -108,7 +90,7 @@ WAV::WAV()
 	bitsPerSample = DEFAULT_BITS_PER_SAMPLE;	
 	assert(!strcpy_s(subchunk2Id, STRING_NUMBER_SYMBOLS_WAV, "data"));
 	subchunk2Size = NULL;
-	// Далее следуют непосредственно Wav данные.
+
 	waveData = nullptr;
 
 	assert(!WAV::count());
@@ -170,7 +152,7 @@ int WAV::write(FILE* output) {
 
 WAV::~WAV() {
 
-	// при дальнейщей работе тут будет полный деструктор
+	// now have no destructor
 
 }
 
